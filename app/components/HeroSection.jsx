2dcc8f34df60ffd29f0d0d1e4f158d90850002c0
Geoffrey <geoffrey_mok2004@hotmail.com>
2024-02-09 01:16:06 +1100

    apos

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className='col-span-7 place-self-center text-center sm:text-left justify-self-start'>
             <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold'>
               <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-300'>
-                Hello, I'm
+                Hello, I&apos;m
                 </span>
                 <br></br>
                 <ExampleComponent/>
