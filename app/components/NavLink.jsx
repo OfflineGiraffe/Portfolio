116c4ed9edd5a6f8069e5e58b67193dbfc7fa0a9
Geoffrey <geoffrey_mok2004@hotmail.com>
2024-03-06 14:50:54 +1100

    Adding animations to the navlinks

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -4,7 +4,9 @@ const NavLink = ({ href, title }) => {
         return (
                 <Link 
                 href={href} 
-                className="block py-2 pl-3 pr-4 text-[white] sm:text-xl rounded md:p-0 hover:text-white">
+                className="block py-2 pl-3 pr-4 text-white sm:text-xl 
+                rounded md:p-0 hover:text-[white] transition duration-500
+                 hover:scale-125  hover:rounded-lg hover:border-sky-500 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
                         {title}
                         
                 </Link>
